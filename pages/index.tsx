a0d9adac0e2a1e956352d0d999e0c5de329f7fa9
Alexandru Ifrim <alexandru.ifrim@gmail.com>
2022-11-14 00:12:05 +0200

    fix: properly load fira code from next google fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,18 @@ import { Fira_Code } from "@next/font/google";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Shell from "../shell";
+import { cls } from "../shell/utils";
 
-const firaCode = Fira_Code({ subsets: ["latin"] });
+const firaCode = Fira_Code({
+    subsets: ["latin"],
+    display: "block",
+    preload: true,
+    variable: "--font",
+});
 
 const Home: NextPage = () => {
     return (
-        <main>
+        <main className={cls(firaCode.variable)}>
             <Head>
                 <title>aifrim.com - shell</title>
                 <meta name="description" content="/home" />
