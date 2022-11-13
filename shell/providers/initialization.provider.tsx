c7479e7b7a743d6a0780bbcddd689c44f5c800c2
Alexandru Ifrim <alexandru.ifrim@gmail.com>
2022-11-13 15:50:38 +0200

    feat: startup update

--- shell/providers/initialization.provider.tsx
+++ shell/providers/initialization.provider.tsx
@@ -14,10 +14,13 @@ const fontLoading = (font: string) =>
         }
     });
 
-const textToInitializationMessage = (text: JSX.Element) => (
+const generateInitializationMessage = (text: JSX.Element, tick = false) => (
     <div className="cmd">
-        <span className="material-symbols-outlined">done</span>
-        <span>&nbsp;{text}</span>
+        {tick ? <span className="material-symbols-outlined">done</span> : null}
+        <span>
+            {tick ? <>&nbsp;</> : null}
+            {text}
+        </span>
     </div>
 );
 
@@ -30,7 +33,50 @@ function useInitializationStore() {
         clear();
         done(false);
 
+        const separator = (
+            <div className="separator">
+                <div>&lt;==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==</div>
+                <div>==&gt;</div>
+            </div>
+        );
+
+        const startupMessages = [
+            <>Welcome to aifrim.com</>,
+            <>&nbsp;</>,
+            <>#&nbsp;Starting application initialization</>,
+            <>#&nbsp;Waiting for necessary packages</>,
+        ];
+
+        for (const message of startupMessages) {
+            addInitializationMessage(generateInitializationMessage(message));
+
+            await sleep(500);
+        }
+
+        addInitializationMessage(separator);
+
         await fontLoading("14px 'Fira Code'");
+
         await fontLoading("14px 'Material Symbols Outlined'");
 
         const messages = [
@@ -41,14 +87,14 @@ function useInitializationStore() {
         ];
 
         for (const message of messages) {
-            addInitializationMessage(textToInitializationMessage(message));
+            addInitializationMessage(
+                generateInitializationMessage(message, true)
+            );
 
             await sleep(1000);
         }
 
-        addInitializationMessage(
-            <div>&lt;==================================&gt;</div>
-        );
+        addInitializationMessage(separator);
 
         done(true);
     }, []);
